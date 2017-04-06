752f6956f951cda7e9c75ac14d24f069217aa86b
awdesh <awdesh.sharma1@gmail.com>
2017-04-05 23:22:24 -0700

    adding initial commit for elevator design

---
+++ elevator/runner.py
@@ -0,0 +1,11 @@
+class ElevatorController:
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.round = 1
+
+    @classmethod
+    def call(cls):
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    # // call the run functionself.
+    print 'main is called'
+
+if __name__ == "__main__":
+    main();
