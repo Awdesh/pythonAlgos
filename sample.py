0f09891c4ea0b6fc8efd79ee2b0b4af0b18fc1c5
awdesh <awdesh.sharma1@gmail.com>
2017-04-17 23:14:36 -0700

    adding collection sample

--- sample.py
+++ sample.py
@@ -1,36 +1,45 @@
 #!/usr/bin/python
 
-str = 'Hello World!'
+class Sample:
+    str = 'Hello World!'
 
-print str          # Prints complete string
-print str * 5
-list = ['aa', 1, 1.0, 131212343241, ]
+    def __init__():
+        print 'inside constructor'
 
-print list[0]
-print list[1]
-print list[2]
+    def print_collection():
+        print str          # Prints complete string
+        print str * 5
+        list = ['aa', 1, 1.0, 131212343241, ]
+
+        print list[0]
+        print list[1]
+        print list[2]
 
 
-print list[3]
+        print list[3]
 
-#  left to the colon is index , right to it is element.
-print list[1:3]
-print list[1:]
-print list[:1]
-print list[:3]
+        #  left to the colon is index , right to it is element.
+        print list[1:3]
+        print list[1:]
+        print list[:1]
+        print list[:3]
 
-dict = {}
-dict['key1'] = 'value1'
-dict[1] = 10
-dict[2.0] = 13123123
+        dict = {}
+        dict['key1'] = 'value1'
+        dict[1] = 10
+        dict[2.0] = 13123123
 
-print dict
+        print dict
 
-var = 500
+        var = 500
 
-if var == 100:
-    print 'inside if'
-elif var == 50:
-    print 'inside elif'
-else:
-    print 'else'
+        if var == 100:
+            print 'inside if'
+        elif var == 50:
+            print 'inside elif'
+        else:
+            print 'else'
+
+
+    if __name__ == '__main__':
+        print()
