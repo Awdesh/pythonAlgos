9d15a27aafc6898deb19d6c2c03ffa3c8a171ec0
awdesh <awdesh.sharma1@gmail.com>
2017-04-20 22:35:51 -0700

    updating anagram method

--- DataStructure/Strings/anagram.py
+++ DataStructure/Strings/anagram.py
@@ -8,6 +8,10 @@ class Anagram:
     def is_anagram(self,s1, s2):
         print '***** starting *****'
 
+        print '***** convert input strings to lowercase'
+        s1 = s1.lower()
+        s2 = s2.lower()
+
         for i in s1:
             if i not in Anagram.dict:
                 Anagram.dict[i] = 1
@@ -20,10 +24,7 @@ class Anagram:
             if i not in Anagram.dict:
                 return false
             else:
-                key = Anagram.dict.get(i)
-                key -= 1
-                # import pdb; pdb.set_trace()
-                Anagram.dict[i] = key
+                Anagram.dict[i] -= 1
 
         print Anagram.dict
 
@@ -37,10 +38,27 @@ class Anagram:
         else:
             return False
 
+    def is_anagram_1(self, str1, str2):
+        str1_list = list(str1)
+        str1_list.sort()
+
+        str2_list = list(str2)
+        str2_list.sort()
+
+        return (str1_list == str2_list)
+
 if __name__ == '__main__':
     a = Anagram()
+    # Test Case-: 1
     b = a.is_anagram('aba', 'bab')
     print b
+    val = a.is_anagram_1('aba', 'bab')
+    print val
+
     o = Anagram()
-    c = o.is_anagram('aba', 'baa')
+    # Test Case-: 2
+    c = o.is_anagram('car', 'arc')
     print c
+    # Test Case-: 3
+    d = o.is_anagram('Aba', 'baa')
+    print d
